e786443025e2932735d0abec50925586cb4a3d21
blackjackal010 <blackjackal010@gmail.com>
2021-08-24 20:04:19 +0530

    Made available for windows

--- xo_main.py
+++ xo_main.py
@@ -3,6 +3,8 @@ import sys
 import time
 
 class Game():
+    """The Game Class"""
+
     def __init__(self):
         self.help_board = [[str(j*3+i+1) for i in range(3)] for j in range(3)]
         self.player_1 = None
@@ -10,13 +12,18 @@ class Game():
         self.scores = [0,0]
         self.help = False
         self.reset_board()
+
     def clear_all(self):
-        os.system('clear')
+        if sys.platform == 'linux':
+            os.system('clear')
+        elif sys.platform == 'win32':
+            os.system('cls')
         print('')
         print(' '*12+'+------------------------------+')
         print(' '*12+'|          X O Game            |')
         print(' '*12+'+------------------------------+')
         print('')
+
     def show_board(self,board):
         for i in range(3):
             print(' '*12+'+-------'*3+'+')
@@ -28,18 +35,23 @@ class Game():
             print(' '*12+'|       '*3+'|')
         print(' '*12+'+-------'*3+'+')
         print('')
+
     def show_menu(self,options,close = 'Back',conds={}):
         keys = list(options.keys())
+
         for i in range(len(keys)):
             if keys[i] in conds:
                 if not conds[keys[i]]:
                     del options[keys[i]]
         options[close] = False
         keys = list(options.keys())
+
         while not options[close]:
             self.clear_all()
+
             for i in range(len(keys)):
                 print(f"{i+1}) {keys[i]}")
+
             try:
                 choice = input("\n\n---> ")
                 if choice == 'q':
@@ -56,14 +68,18 @@ class Game():
                     options[close] = True
             except:
                 continue
+    
     def load(self):
         self.feature_coming_soon()
+    
     def save(self):
         pass
+    
     def feature_coming_soon(self):
         self.clear_all()
         print('feature coming soon')
         time.sleep(1)
+    
     def nav(self,goto):
         self.mode = goto
         if goto == 'singleplay':
@@ -74,10 +90,13 @@ class Game():
             self.samescreen()
         elif goto == 'loadgame':
             self.load()
+    
     def singleplay(self): # single player mode with AI
         pass
+    
     def localmultiplay(self): # Local network Multiplayer
         pass
+    
     def samescreen(self): # Same screen Multiplayer
         self.clear_all()
         self.show_board(self.help_board)
@@ -121,6 +140,7 @@ class Game():
                 
                 print('Please Type number between 1 to 9')
                 time.sleep(0.5)
+    
     def who_win(self):
         for i in range(3):
             if self.board[i][0] == self.board[i][1] and self.board[i][0] == self.board[i][2] and self.board[0][i] != ' ':
@@ -133,6 +153,7 @@ class Game():
             return self.board[0][2]  
         else:
             return ''
+    
     def show_win(self,player):
         self.clear_all()
         self.show_board(self.board)
@@ -145,11 +166,13 @@ class Game():
         self.reset_board()
         if choice == 'q':
             sys.exit()
+    
     def reset_board(self):
         self.board = [[' ' for i in range(3)] for j in range(3)]
         if self.help:
             self.board = self.help_board
         self.turn = 0
+    
     def check_board(self):
         win = self.who_win()
         if win == 'X':
@@ -166,11 +189,13 @@ class Game():
             print("draw")
             self.reset_board()
             self.swap()        
+    
     def swap(self):
         c = self.player_1
         self.player_1 = self.player_2
         self.player_2 = c
         self.scores = self.scores[::-1]    
+    
     def start(self):
         self.show_menu(options={
             'New Game': {
@@ -185,6 +210,7 @@ class Game():
         close = 'Quit', 
         conds={'Continue Game': os.path.isfile('game.json')})
         
+
 
 
 if __name__ == "__main__":
